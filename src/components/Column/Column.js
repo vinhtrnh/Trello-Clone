fc8928b436d659ce5c4bbe0e30dcb312c3987989
Drake <trinhminh1990@gmail.com>
2022-12-18 17:15:50 +0700

    implement_drag_and_drop

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,23 +1,51 @@
-import Card from "components/Card/Card";
-import React from "react";
+import Card from 'components/Card/Card'
+import React from 'react'
+import { Container, Draggable } from 'react-smooth-dnd'
 
-import './Column.scss';
+import './Column.scss'
 import { mapOrder } from 'utilities/sorts'
 function Column(props) {
-    const { column } = props
-    const cards = mapOrder(column.cards, column.cardOrder, 'id')
+  const { column } = props
+  const cards = mapOrder(column.cards, column.cardOrder, 'id')
 
-    return (
-        <div className='column'>
-            <header>{column.title}</header>
-            <ul className="card-list">
-                {cards.map((card, index) => <Card key={index} card={card} />)}
-            </ul>
-            <footer>Add new card</footer>
-        </div>
+  const onCardDrop = (dropResult) => {
+    console.log(dropResult)
+  }
+  return (
+    <div className='column'>
+      <header className='column-drag-handle'>{column.title}</header>
+      <div className="card-list">
+
+        <Container
+
+          orientation="vertical"
+          groupName="col"
+          onDrop={onCardDrop}
+          getChildPayload={index => cards[index]}
+          dragClass='card-ghost'
+          dropClass='card-ghost-drop'
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: 'cards-drop-preview'
+          }}
+          dropPlaceholderAnimationDuration={200}
+        >
+
+          {cards.map((card, index) => (
+            <Draggable key={index}>
+              <Card card={card} />
+            </Draggable>
+          ))}
+
+        </Container>
+
+      </div>
+      <footer>Add new card</footer>
+    </div>
 
 
-    )
+  )
 }
 
 export default Column
