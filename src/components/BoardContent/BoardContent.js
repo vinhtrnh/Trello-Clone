ff55f066c422397cf6b3bb81a39f88c7715b9839
Drake <trinhminh1990@gmail.com>
2022-12-19 14:55:22 +0700

    handle_state_change

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -5,6 +5,7 @@ import './BoardContent.scss'
 import { initialData } from 'actions/initialData'
 import { isEmpty } from 'lodash'
 import { mapOrder } from 'utilities/sorts'
+import { applyDrag } from 'utilities/dragDrop'
 
 function BoardContent() {
   const [board, setBoard] = useState({})
@@ -32,7 +33,27 @@ function BoardContent() {
     return <div className='not-found'> Board Not Found!</div>
   }
   const onColumnDrop = (dropResult) => {
-    console.log(dropResult)
+    let newColumns = [...columns]
+    newColumns = applyDrag(newColumns, dropResult)
+
+    let newBoard = { ...board }
+    newBoard.columnOrder = newColumns.map(c => c.id)
+    newBoard.columns = newColumns
+
+    setColumns(newColumns)
+    setBoard(newBoard)
+  }
+  const onCardDrop = (columnId, dropResult) => {
+    if (dropResult.removedIndex !== null || dropResult.addedIndex !== null ) {
+      let newColumns = [...columns]
+      let currentColumn = newColumns.find(c => c.id === columnId )
+
+      currentColumn.cards = applyDrag( currentColumn.cards, dropResult)
+      currentColumn.cardOrder = currentColumn.cards.map(i => i.id)
+
+      setColumns(newColumns)
+    }
+
   }
   return (
     <div className='board-content'>
@@ -50,11 +71,14 @@ function BoardContent() {
       >
         {columns.map((column, index) => (
           <Draggable key={index}>
-            <Column column={column} />
+            <Column column={column} onCardDrop={onCardDrop} />
           </Draggable>
         ))}
 
       </Container>
+      <div className='add-new-column'>
+        <i className='fa fa-plus icon'></i>Add new column
+      </div>
     </div>
   )
 }
